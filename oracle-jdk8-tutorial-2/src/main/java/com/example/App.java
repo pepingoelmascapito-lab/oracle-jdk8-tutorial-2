7688d56f8f227c5d7461844e085b67cc7035cf2a
pepingoelmascapito <pepingoelmascapito@gmail.com>
2026-05-25 18:13:20 +0200

    25 de mayo patron builder de lombox explicado ;v

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -20,7 +20,28 @@ public class App {
 		// Manzana, me daria error porque yo no tengo un constructor que reciba un solo
 		// parametro
 
-		Manzana manzana2 = new Manzana("Golden");
+		/*
+		 * Utilizando Lombok no necesitamos tener todos los constructores definidos,
+		 * solamente el constructor sin parametros o por defecto y el constructor que
+		 * recibe todos los campos de la clase.
+		 * 
+		 * UTILIZANDO EL PATRON BUILDER DE LOMBOK, es posible construir cualquier objeto
+		 * aunque no tengas su constructor explicitamente, y tampoco estariamos obligado
+		 * a suministrar los campos o propiedades del objeto en un orden determinado
+		 * 
+		 * EL patron builder no fue inventado por lombok, pero lo ha mejorado bastante
+		 */
+
+		// El Patron Builder NO fue inventado por lombok, pero lo ha mejorado bastante
+		
+		
+		
+		
+		//Manzana manzana2 = new Manzana("Golden");
+		
+		Manzana manzana2 = Manzana.builder()
+				.variedad("Golden")
+				.build();
 
 		System.out.println("------- Imprimiendo manzana2 ------- ");
 		System.out.println(manzana2);
@@ -31,8 +52,19 @@ public class App {
 		 * Vamos a crear dos objetos Manzana que sean de la misma variedad y precio
 		 */
 
-		Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
-		Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
+		//Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
+		
+		Manzana manzana3 = Manzana.builder()
+				.variedad("reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+		
+		Manzana manzana4 = Manzana.builder()
+				.variedad("reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+		
+		//Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
 
 		// Si quiero comprobar si las variables x e y son iguales, podemos utilizar una
 		// sentencia de Java
@@ -60,19 +92,13 @@ public class App {
 		 * equal, porque el operador == lo que comprueba en el caso de objetos es si
 		 * dichos objetos estan en la misma ubicacion de la memoria
 		 */
-		
+
 		if (manzana3.equals(manzana4)) {
 			System.out.println("El contenido de ambos objeto es el mismo");
-			} else {
+		} else {
 			System.out.println("El contenido de ambos objeto es diferente");
-		
+
+		}
 
 	}
-
-		
-		
-		
-		
-		
-}
 }
--- oracle-jdk8-tutorial-2/src/main/java/com/example/Manzana.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/Manzana.java
@@ -3,6 +3,17 @@ package com.example;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data @Builder
+
+
+
 public class Manzana {
 	
 	/* Una clase bien diseñada es aquella que tiene todas sus propiedades
@@ -19,168 +30,10 @@ public class Manzana {
 	private double peso;
 	private BigDecimal precio;
 	
-	// Los contructores (Metodos que tienen el mismo nombre que la clase)
-	// y que son los que permiten crear o instanciar los objetos de este tipo de clase	
-	
-	public Manzana(String variedad, BigDecimal precio) {
-		super();
-		this.variedad = variedad;
-		this.precio = precio;
-	}
-
-
-
-	public Manzana() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	public Manzana(String color, String sabor, String variedad) {
-		super();
-		this.color = color;
-		this.sabor = sabor;
-		this.variedad = variedad;
-	}
-
-
-
-	public Manzana(String color, String sabor, String variedad,
-			double size, double peso, BigDecimal precio) {
-		super();
-		this.color = color;
-		this.sabor = sabor;
-		this.variedad = variedad;
-		this.size = size;
-		this.peso = peso;
-		this.precio = precio;
-	}
-
-
-
-	public Manzana(String variedad) {
-		super();
-		this.variedad = variedad;
-	}
-
-	// Metodos getters and setters (metodos publicos para manejar las propiedades privadas)
-
-	public String getColor() {
-		return color;
-	}
-
-
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-
-
-	public String getSabor() {
-		return sabor;
-	}
-
-
-
-	public void setSabor(String sabor) {
-		this.sabor = sabor;
-	}
-
-
-
-	public String getVariedad() {
-		return variedad;
-	}
-
-
-
-	public void setVariedad(String variedad) {
-		this.variedad = variedad;
-	}
-
-
-
-	public double getSize() {
-		return size;
-	}
-
-
-
-	public void setSize(double size) {
-		this.size = size;
-	}
-
-
-
-	public double getPeso() {
-		return peso;
-	}
-
-
-
-	public void setPeso(double peso) {
-		this.peso = peso;
-	}
-
-
-
-	public BigDecimal getPrecio() {
-		return precio;
-	}
-
-
-
-	public void setPrecio(BigDecimal precio) {
-		this.precio = precio;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "Manzana [color=" + color + ", sabor=" + sabor + ", variedad=" + variedad + ", size=" + size + ", peso="
-				+ peso + ", precio=" + precio + "]";
-	}
-
-
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(precio, variedad);
-	}
-
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Manzana other = (Manzana) obj;
-		return Objects.equals(precio, other.precio) && Objects.equals(variedad, other.variedad);
-	}
-
-
-
 	
 
-	// Otros metodos (No estan asociados a ninguna propiedad concreta, sino a varias)
-	
-	//aqui, 78787
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
 	
 }	
 	
