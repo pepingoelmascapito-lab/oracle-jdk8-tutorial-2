02d2308e8ee57c4c84a25f36d684d77203bdb87f
pepingoelmascapito <pepingoelmascapito@gmail.com>
2026-06-04 17:49:37 +0200

    sentencia while, caso normal

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -666,13 +666,33 @@ public class App {
 									
 		// sentencia while
 		
+		/* Sentencia while:
+		 * 
+		 *  Las sentencias for clasica y for mejorada se utilizan cuando se sabe,
+		 *  a priori, la cantidad de veces que se va a ejecutar un bloque de 
+		 *  instrucciones. 
+		 *  
+		 *   Mientras que la sentencia while se utiliza cuando no se sabe, a priori, 
+		 *   la cantidad de veces que se va a ejecutar un bloque de instrucciones y
+		 *   ademas, cuando no se requiere ejecutar el bloque de instrucciones ni 
+		 *   una sola vez aunque ya no se cumpla la condicion*/
 		
+		// Ejemplo sencillo para comprender la sintaxis de la sentencia while
 		
+		int z = 10;
+		int counterr = 0;
 		
+		while (z >= 0) {
+			System.out.println("El valor de la variable z: " + z);
+			z--;
+			counterr++;
+		}
+		System.out.println("El bloque correspondiente a la sentencia while" + 
+				", se ha ejecutado " + counterr + " veces");
+		System.out.println("El valor final de la variable z es: " + z);
 		
+	
 		
-	}
-
 	/*
 	 * SENTENCIA SWITCH CASE.
 	 * 
@@ -696,7 +716,7 @@ public class App {
 	 * 
 	 * Segundo: con una sentencia switch - case en el cuerpo del metodo
 	 */
-
+	}
 	static void infoDiaSemana(DiaSemana diaSemana) {
 
 		// Utilizando sentencia switch - case
